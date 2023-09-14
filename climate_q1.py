602dba8e65d606cee66d9dfd56a60118187201e0
William-7777 <williamgaribaldi@outlook.com>
2023-09-14 11:36:30 +1000

    wrote some code to connect to database, fetch values, and populate lists with values

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,22 @@
+
 import matplotlib.pyplot as plt
-        
+#Start
+import sqlite3
+
+con = sqlite3.connect('climate.db')
+cursor = con.cursor()
+cursor.execute("SELECT * FROM ClimateData")# Returns array of tuples
+
 years = []
 co2 = []
 temp = []
+
+#grabs the relevant index of each tuple to populate lists
+for tuple_row in cursor.fetchall():
+    years.append(tuple_row[0])
+    co2.append(tuple_row[1])
+    temp.append(tuple_row[2])
+#end of changes
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
